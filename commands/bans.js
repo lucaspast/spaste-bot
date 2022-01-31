46e13509d4a1d23c8eeb1e83276394d2a0fe2b5c
lucaspast <56891338+lucaspast@users.noreply.github.com>
2022-01-31 11:15:10 +0100

    bans slash
    integration of slash command for bans

--- commands/bans.js
+++ commands/bans.js
@@ -1,14 +1,19 @@
 
-exports.run = async function (client, message, args) {
-    if (!message.member.hasPermission('BAN_MEMBERS'))
-        return message.channel.send({ embed: { color: "RED", description: "You can't use this command!" } });
-    message.guild.fetchBans()
+exports.run = async function (interaction) {
+    if (!interaction.member.hasPermission('BAN_MEMBERS'))
+        return interaction.reply({ embed: { color: "RED", description: "You can't use this command!" } });
+    interaction.guild.fetchBans()
         .then(banned => {
             let list = banned.map(ban => `${ban.user.tag} : ${ban.user.id}`).join('\n');
 
             if (list.length >= 1950) list = `${list.slice(0, 1948)}...`;
 
-            message.channel.send(`**${banned.size} users are banned:**\n${list}`);
+            interaction.reply(`**${banned.size} users are banned:**\n${list}`);
         })
         .catch(console.error);
 };
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+exports.slashcmdInfo = new SlashCommandBuilder()
+    .setName('bans')
+    .setDescription('View the list of banned members!');
